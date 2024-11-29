4637c69b91d5a8aa2b2c0434e659705bd86cdba7
Daniel Frystacky <danielfrystacky@gmail.com>
2024-11-28 22:15:30 -0500

    added sandbox scense and created grab scripts to grap objects

---
+++ movieTheaterSim/Assets/Scripts/PlayerPickUpDrop.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class PlayerPickUpDrop : MonoBehaviour
+{
+    [SerializeField] private Transform playerCameraTransform;
+    [SerializeField] private LayerMask pickUpLayerMask;
+    [SerializeField] private Transform objectGrabPointTransform;
+
+    private ObjectGrabbable objectGrabbable;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            //not carrying object, try to grab one
+            if (objectGrabbable == null)
+            {
+                float pickUpDistance = 2f;
+                if (Physics.Raycast(playerCameraTransform.position, playerCameraTransform.forward, out RaycastHit raycastHit, pickUpDistance, pickUpLayerMask))
+                {
+                    //Debug.Log(raycastHit.transform);
+                    if (raycastHit.transform.TryGetComponent(out objectGrabbable))
+                    {
+                        objectGrabbable.Grab(objectGrabPointTransform);
+                        //Debug.Log(objectGrabbable);
+                    }
+                }
+            }
+            else
+            {
+                // carrying a grabbed object, drop it
+                objectGrabbable.Drop();
+                objectGrabbable = null;
+            }
+            
+        }
+    }
+}
