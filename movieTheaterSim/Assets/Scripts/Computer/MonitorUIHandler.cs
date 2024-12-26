0fdc2733802688b79c364172c2349470fe5200ba
Daniel Frystacky <danielfrystacky@gmail.com>
2024-12-26 12:43:39 -0500

    work in progress on pc ui

---
+++ movieTheaterSim/Assets/Scripts/Computer/MonitorUIHandler.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class MonitorUIHandler : MonoBehaviour
+{
+    public void test()
+    {
+        Debug.Log("TESTING 1 2 3 ...");
+    }
+
+
+    private void Start()
+    {
+        Debug.Log("Running UI...");
+    }
+
+
+}
