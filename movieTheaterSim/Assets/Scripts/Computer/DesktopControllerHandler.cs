3a817053de85980e9679a4d06a104d0d14f19af2
Daniel Frystacky <danielfrystacky@gmail.com>
2024-12-31 14:32:00 -0500

    added change of cursor at pc location

--- movieTheaterSim/Assets/Scripts/Computer/DesktopControllerHandler.cs
+++ movieTheaterSim/Assets/Scripts/Computer/DesktopControllerHandler.cs
@@ -8,6 +8,7 @@ using static UnityEditor.Experimental.GraphView.GraphView;
 public class DesktopControllerHandler : MonoBehaviour
 {
     public Transform mouseCenter;
+    public RectTransform mouseCenterRect;
     public Transform PCViewPos;
     public GameObject player;
     public float mouseSensitivity;
@@ -17,6 +18,11 @@ public class DesktopControllerHandler : MonoBehaviour
     private bool isMoving = false;
     //used to control the speed at which the camera turns at pc
     [SerializeField] private float anglePerSecond = 25.0f;
+
+    public Texture2D hoverCursor;
+
+    public Rigidbody rb;
+    public Transform tf;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -30,12 +36,6 @@ public class DesktopControllerHandler : MonoBehaviour
         //float x = Mouse.current.position.ReadValue().x;
         //float y = Mouse.current.position.ReadValue().y;
         //transform.localPosition = new Vector3(-x * mouseSensitivity,-y * mouseSensitivity, y);
-
-        Vector3 newPos = mouseCenter.position;
-        newPos.x = Mathf.Clamp(newPos.x, mouseCenter.position.x - maxXDistance, mouseCenter.position.x - maxXDistance);
-        newPos.y = Mathf.Clamp(newPos.y, mouseCenter.position.y - maxXDistance, mouseCenter.position.y - maxXDistance);
-
-        //transform.position = newPos;
 
         if (Input.GetKeyDown(KeyCode.G))
         {
@@ -57,6 +57,9 @@ public class DesktopControllerHandler : MonoBehaviour
                 isMoving = false;
                 //resets the local pos of camera back to player, since orginal is 0.0.0 under first person controller
                 player.GetComponent<FirstPersonController>().playerCamera.transform.localPosition = new Vector3(0, 0, 0);
+                
+                //TODO: figure out how to get playerx cursor only
+                UnityEngine.Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
                 //allows the first person controller to move again
                 player.GetComponent<FirstPersonController>().enabled = true;
             }
@@ -65,6 +68,7 @@ public class DesktopControllerHandler : MonoBehaviour
                 //turns off movement for first person controller character
                 player.GetComponent<FirstPersonController>().enabled = false;
                 //turns on movement of player camera to target pos
+                
                 isMoving = true;
             }
 
@@ -78,18 +82,12 @@ public class DesktopControllerHandler : MonoBehaviour
 
             //Rotates to target location smoothly over time
             player.GetComponent<FirstPersonController>().playerCamera.transform.rotation = Quaternion.RotateTowards(player.GetComponent<FirstPersonController>().playerCamera.transform.rotation, PCViewPos.transform.rotation, anglePerSecond * Time.deltaTime);
-            
-            //quickly moves and rotates to the target pos
-            //player.GetComponent<FirstPersonController>().playerCamera.transform.SetPositionAndRotation(PCViewPos.transform.position, PCViewPos.transform.rotation);
-        }
 
-        //TODO
-        //when interaction mode, mouse movement will move computer mouse movement
-        //Clamp mouse to computer screen
-        //have mouse able to interact with stuff on computer screen
-        //create some computer logic, maybe buy items?
-        //add more protoype UI to computer, time? money? etc..
-        //have logic to be able to interact with computer, it will get player info and etc so it can tell who is player 1 and 2
+            if(player.GetComponent<FirstPersonController>().playerCamera.transform.position == PCViewPos.transform.position)
+            {
+                UnityEngine.Cursor.SetCursor(hoverCursor, Vector2.zero, CursorMode.Auto);
+            }
+        }
 
     }
 }
