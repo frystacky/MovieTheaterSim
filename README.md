ce8ed2f273281dfb61ee65fc6ecf4616b30c23d1
Daniel Frystacky <danielfrystacky@gmail.com>
2025-01-03 12:38:53 -0500

    a complete prototype of desktop object

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # MovieTheaterSim
+
+## Current Keys ##
+keys can be changed and adjusted in engine by going in to: Edit -> Project Settings... -> Input Manager
+
+**Current keys**<br />
+PickUp = e<br />
+Throw = mouse 1<br />
+Interaction = i<br />
--- movieTheaterSim/Assets/Scripts/Computer/DesktopControllerHandler.cs
+++ movieTheaterSim/Assets/Scripts/Computer/DesktopControllerHandler.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using TMPro;
 using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
@@ -9,11 +10,13 @@ using static UnityEditor.Experimental.GraphView.GraphView;
 public class DesktopControllerHandler : MonoBehaviour
 {
     [SerializeField] private Transform PCViewPos; //position of point where player gets moved to
-    public GameObject player;  //TODO get dynamically later
-    private bool isMoving = false;
+    [SerializeField] private GameObject player; //the interacting player object
+    [SerializeField] private Texture2D hoverCursor; //mouse cursor icon that gets switch when viewing pc
+    [SerializeField] private TextMeshProUGUI clockText; //clock text in taskbar - TODO: set to gameTime player
     [SerializeField] private float anglePerSecond = 25.0f; //used to control the speed at which the camera turns at pc
-    [SerializeField] public Texture2D hoverCursor; //mouse cursor icon that gets switch when viewing pc
-    [SerializeField] private TextMeshProUGUI clockText;
+    [SerializeField] private float camMoveSpeed = 3f; //rate at which the cam moves to the object
+
+    private bool isMoving = false;
 
     [Header("Button Gameobject to fetch by name")]
     [SerializeField] private GameObject pcIcons;
@@ -23,37 +26,20 @@ public class DesktopControllerHandler : MonoBehaviour
     [SerializeField] private GameObject bankApp;
     [SerializeField] private GameObject testApp3;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        //UnityEngine.Cursor.SetCursor(hoverCursor, Vector2.zero, CursorMode.Auto);
-    }
+    private bool canInteract = false;
 
     // Update is called once per frame
     void Update()
     {
-
-        if (Input.GetKeyDown(KeyCode.G))
+        if (Input.GetButtonDown("Interaction") && canInteract && player != null)
         {
-            //foreach (Transform childTransform in player.transform)
-            //{
-            //    if(childTransform.tag == "MainCamera")
-            //    {
-            //        Debug.Log("Find maincam!!");
-            //        playersCamLocation = childTransform.gameObject;
-            //    }
-            //}
-            
-            Debug.Log("Turning off player Controller");
-
-
             if (player.GetComponent<FirstPersonController>().enabled == false)
             {
                 //stops the update of moving to target
                 isMoving = false;
                 //resets the local pos of camera back to player, since orginal is 0.0.0 under first person controller
                 player.GetComponent<FirstPersonController>().playerCamera.transform.localPosition = new Vector3(0, 0, 0);
-                
+
                 //TODO: figure out how to get playerx cursor only
                 UnityEngine.Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
                 //allows the first person controller to move again
@@ -66,14 +52,12 @@ public class DesktopControllerHandler : MonoBehaviour
                 //turns on movement of player camera to target pos              
                 isMoving = true;
             }
-
-
         }
 
-        if(isMoving && player != null)
+        if (isMoving && player != null)
         {
             //moves to target location smoothly over time
-            player.GetComponent<FirstPersonController>().playerCamera.transform.position = Vector3.MoveTowards(player.GetComponent<FirstPersonController>().playerCamera.transform.position, PCViewPos.transform.position, 3f * Time.deltaTime);
+            player.GetComponent<FirstPersonController>().playerCamera.transform.position = Vector3.MoveTowards(player.GetComponent<FirstPersonController>().playerCamera.transform.position, PCViewPos.transform.position, camMoveSpeed * Time.deltaTime);
 
             //Rotates to target location smoothly over time
             player.GetComponent<FirstPersonController>().playerCamera.transform.rotation = Quaternion.RotateTowards(player.GetComponent<FirstPersonController>().playerCamera.transform.rotation, PCViewPos.transform.rotation, anglePerSecond * Time.deltaTime);
@@ -86,8 +70,28 @@ public class DesktopControllerHandler : MonoBehaviour
 
         //change this later to actual game time
         clockText.text = System.DateTime.Now.ToString("h:mm tt");
-
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+       if(other.gameObject.CompareTag("Player"))
+       {
+            canInteract = true;
+            player = other.gameObject;
+        }
+  
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            canInteract = false;
+            player = null;
+        }
+    }
+
+    /// |*************************| UI LOGIC |*************************|
 
     private void ClickOnIcon()
     {
