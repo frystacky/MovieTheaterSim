2b1c08368c1a38f8b93f373a3e183963936713c8
Daniel Frystacky <danielfrystacky@gmail.com>
2025-01-12 11:58:59 -0500

    fixed picked up object being jittery and other bugs

--- movieTheaterSim/Assets/Scripts/ObjectGrabbable.cs
+++ movieTheaterSim/Assets/Scripts/ObjectGrabbable.cs
@@ -6,52 +6,65 @@ public class ObjectGrabbable : MonoBehaviour
     private Rigidbody objectRigidbody;
     public Transform objectGrabPointTransform;
     [SerializeField] private float throwForce = 500f; //force at which the object is thrown at
+    Vector3 globalScale;
 
     private void Start()
     {
         objectRigidbody = GetComponent<Rigidbody>();
+        globalScale = this.transform.lossyScale;
     }
     public void Grab(Transform objectGrabPointTransform, GameObject player)
     {
         this.objectGrabPointTransform = objectGrabPointTransform;
         this.player = player;
-        objectRigidbody.useGravity = false;
+        objectRigidbody.isKinematic = true;
         objectRigidbody.transform.parent = objectGrabPointTransform; //add the picked up object as a child to ObjectGrabPoint
-        Physics.IgnoreCollision(objectRigidbody.GetComponent<Collider>(), player.GetComponent<Collider>(), true); //turn off collistion with player
-        transform.rotation = Quaternion.identity;
+        Physics.IgnoreCollision(objectRigidbody.GetComponent<Collider>(), player.GetComponent<Collider>(), true); //turn off collistion with playern
+        transform.localRotation = Quaternion.identity;  //resets rotations on pick up to center object
+        transform.localPosition = Vector3.zero;         //reset postion on pick up to be in pick up pos
         this.player = null;
     }
 
     public void Drop(GameObject player)
     {
         this.objectGrabPointTransform = null;
-        objectRigidbody.useGravity = true;
+        objectRigidbody.isKinematic = false;
         objectRigidbody.transform.parent = null;
         Physics.IgnoreCollision(objectRigidbody.GetComponent<Collider>(), player.GetComponent<Collider>(), false);
-        transform.rotation = Quaternion.Euler(0f, 0f, 0f); //trys and make sure the object drops straight down
+        //transform.rotation = Quaternion.Euler(0f, 0f, 0f); //trys and make sure the object drops straight down
         this.player = null;
+        this.transform.localScale = new Vector3(1, 1, 1);
     }
 
     public void ThrowObject(GameObject player)
     {
         //same as drop function, but adds force to object before undefining it
-        this.objectGrabPointTransform = null;
-        objectRigidbody.useGravity = true;
+        this.objectGrabPointTransform = null;   
         objectRigidbody.transform.parent = null;
+        objectRigidbody.isKinematic = false;
         Physics.IgnoreCollision(objectRigidbody.GetComponent<Collider>(), player.GetComponent<Collider>(), false);
         objectRigidbody.AddForce(transform.forward * throwForce);
-        transform.rotation = Quaternion.Euler(0f, 0f, 0f); //trys and make sure the object drops straight down
+        //transform.rotation = Quaternion.Euler(0f, 0f, 0f); //trys and make sure the object drops straight down
         this.player = null;
+        this.transform.localScale = new Vector3(1, 1, 1);
     }
 
-    private void FixedUpdate()
+    //private void FixedUpdate()
+    //{
+    //    //if (objectGrabPointTransform != null)
+    //    //{
+    //    //    float lerpSpeed = 10f;
+    //    //    Vector3 newPosition = Vector3.Lerp(transform.position, objectGrabPointTransform.position, Time.deltaTime * lerpSpeed);
+    //    //    objectRigidbody.MovePosition(newPosition);
+    //    //    transform.rotation = Quaternion.Lerp(transform.rotation,objectGrabPointTransform.rotation, Time.deltaTime * lerpSpeed);
+    //    //}       
+    //}
+
+    private void Update()
     {
         if (objectGrabPointTransform != null)
         {
-            float lerpSpeed = 10f;
-            Vector3 newPosition = Vector3.Lerp(transform.position, objectGrabPointTransform.position, Time.deltaTime * lerpSpeed);
-            objectRigidbody.MovePosition(newPosition);
-            transform.rotation = Quaternion.Lerp(transform.rotation,objectGrabPointTransform.rotation, Time.deltaTime * lerpSpeed);
-        } 
+            transform.localScale = new Vector3(1 / objectGrabPointTransform.transform.root.transform.localScale.x, 1 / objectGrabPointTransform.transform.root.transform.localScale.y, 1 / objectGrabPointTransform.transform.root.transform.localScale.z);
+        }
     }
 }
---
+++ movieTheaterSim/Assets/Scripts/SodaMachine/SodaHandler.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class SodaHandler : MonoBehaviour
+{
+    private Transform objectGrabPointTransform;
+    private ObjectGrabbable objectGrabbable;
+
+    [Header("Supply object configurations")]
+    [SerializeField] private float sodaPlaceDistance = 3f; //The distance the grabbed object needs to to to supply items
+    LayerMask layerMask; //layer needed on the object that is able to take supplys
+    private Transform hitItemObjectHandler; // will be set by raycast, the object able to be supplied
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        objectGrabbable = GetComponent<ObjectGrabbable>();
+        layerMask = LayerMask.GetMask("Soda");
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Update()
+    {
+        if (Input.GetButtonDown("Supply"))
+        {
+            this.objectGrabPointTransform = objectGrabbable.objectGrabPointTransform;
+            //checks if player is holding an item and the item is able to supply objects
+            if (objectGrabPointTransform != null)
+            {
+                //cast ray to see if you hit shelf and then if box has item try and add to self
+                if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out RaycastHit raycastHit, sodaPlaceDistance, layerMask))
+                {
+                    Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * raycastHit.distance, Color.green, 3f);
+
+                    if (raycastHit.transform.TryGetComponent(out hitItemObjectHandler))
+                    {
+                        Debug.Log(hitItemObjectHandler.gameObject.name + " HIT2");
+                        objectGrabbable.Drop(this.gameObject);
+                        this.transform.position = hitItemObjectHandler.transform.position;
+                    }
+                }
+                else
+                {
+                    Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.red, 3f);
+                    Debug.Log("Did not Hit");
+                }
+            }
+        }
+    }
+}
